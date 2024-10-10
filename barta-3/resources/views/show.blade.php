ba4fd27ce822206d68c53f437e76b83cd28a50c6
Md. Arif Hassan <aarifhsn@gmail.com>
2024-10-10 13:21:21 +0600

    updated success message

--- barta-3/resources/views/show.blade.php
+++ barta-3/resources/views/show.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+@if(session('success'))
+  <div
+    class="alert alert-success p-2.5 text-sm font-bold text-slate-500 border-2  border-slate-200  rounded-xl inline-block">
+    {{ session('success') }}
+  </div>
+@endif
 
 <main class="container max-w-xl mx-auto space-y-8 mt-8 px-2 md:px-0 min-h-screen">
 
